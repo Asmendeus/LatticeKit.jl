2f031fb28397605b09f5c9019c02e4090bc07ac8
Asmendeus <1422805255@qq.com>
2025-12-13 14:11:34 +0800

    Update getAllPairs.jl

--- src/Crystal/Square/getAllPairs.jl
+++ src/Crystal/Square/getAllPairs.jl
@@ -3,19 +3,39 @@ function getAllPairs(latt::SquareLattice{L, W}, v::NTuple{2, Int64}, which_from:
     @assert which_to == 1 "Out of the range of subcell: `which_to = $which_to`"
 
     pairs = NTuple{2, Int64}[]
-    if boundary isa PBC
+    if (boundary isa PBC) || (boundary isa MBC && boundary == MBC((PBC(), PBC())))
         for site in latt.sites
             site_v = mod(site + v, (L, W))
             if site_v in latt.sites
                 push!(pairs, (getSite(latt, site), getSite(latt, site_v)))
             end
         end
-    elseif boundary isa OBC
+    elseif (boundary isa OBC) || (boundary isa MBC && boundary == MBC((OBC(), OBC())))
         for site in latt.sites
             site_v = site + v
             if site_v in latt.sites
                 push!(pairs, (getSite(latt, site), getSite(latt, site_v)))
             end
+        end
+    elseif boundary isa MBC
+        if boundary == MBC((PBC(), OBC()))
+            for site in latt.sites
+                tmp = site + v
+                site_v = BasicSite((mod(tmp.coord[1], L), tmp.coord[2]), tmp.subcell)
+                if site_v in latt.sites
+                    push!(pairs, (getSite(latt, site), getSite(latt, site_v)))
+                end
+            end
+        elseif boundary == MBC((OBC(), PBC()))
+            for site in latt.sites
+                tmp = site + v
+                site_v = BasicSite((tmp.coord[1], mod(tmp.coord[2], W)), tmp.subcell)
+                if site_v in latt.sites
+                    push!(pairs, (getSite(latt, site), getSite(latt, site_v)))
+                end
+            end
+        else
+            throw(ArgumentError("Undefined behavior of `boundary = $boundary`"))
         end
     else
         throw(ArgumentError("Undefined behavior of `boundary = $boundary`"))
